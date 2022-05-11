26cd617507f2a24bcd78d7b163db01e52ec50d12
James Booker <james.e.booker@gmail.com>
2022-05-11 15:28:39 -0700

    added file and transfer

---
+++ input.js
@@ -0,0 +1,23 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+process.stdin.on("data",(key) =>{
+  if(key === '\u0003'){
+    process.exit();
+  }
+ conn.write(key)
+  });
+ 
+};
+setupInput()
+
+module.exports = {
+  setupInput,
+}
--- play.js
+++ play.js
@@ -1,23 +1,9 @@
 //server 
 
-const {net, connect, } = require("./client");
+const {connect } = require("./client");
+const {setupInput} = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+console.log("Connecting ...");
+connect();
 
-const handleUserInput = function () {
-process.stdin.on("data",(key) =>{
-  if(key === '\u0003'){
-    process.exit();
-  }
- conn.write(key)
-  });
- 
-};
-setupInput()
+setupInput;
